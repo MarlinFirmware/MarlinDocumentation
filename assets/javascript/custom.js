039563b7c0335a8dbfca303835267e1b52cd2347
jbrazio <jbrazio@gmail.com>
2016-03-11 14:02:40 +0000

    Templates can now override the TOC selectors when generating an article

--- assets/javascript/custom.js
+++ assets/javascript/custom.js
@@ -1,35 +1,12 @@
 $( document ).ready(function() {
     $('#toc').toc({
-        'selectors': 'h1,h2,h3',    //elements to use as headings
-        'container': 'body',        //element to find all selectors in
+        'selectors':        (typeof  toc_selectors != 'undefined') ? toc_selectors : 'h1,h2,h3',
+        'activeClass':      'active',
+        'prefix':           'toc-',
 
-        'activeClass':  'active',       //prefix for anchor tags and class names
-        'prefix':       'toc-',         //prefix for anchor tags and class names
+        'scrollToOffset':   70,
+        'highlightOffset':  70,
 
-        'smoothScrolling': function(target, options, callback) {
-            $(target).smoothScroller({
-                offset: options.scrollToOffset
-                }).on('smoothScrollerComplete', function() {
-                    callback();
-                });
-            },
-
-        'onHighlight': function(el) {}, //called when a new section is highlighted
-        'highlightOnScroll': true,      //add class to heading that is currently in focus
-        'highlightOffset': 70,          //offset to trigger the next headline
-        'scrollToOffset': 70,
-
-        //custom function for anchor name
-        'anchorName': function(i, heading, prefix) {
-            return prefix + i;
-        },
-
-        //custom function building the header-item text
-        'headerText': function(i, heading, $heading) {
-            return $heading.text();
-        },
-
-        // custom function for item class
         'itemClass': function(i, heading, $heading, prefix) {
             return 'clearfix ' + prefix + $heading[0].tagName.toLowerCase();;
         }
