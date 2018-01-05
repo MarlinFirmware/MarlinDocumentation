980a186f2ec1745a7344e32f9f337ec46604568f
Sineos <monk@gmx.net>
2018-01-05 17:32:30 +0100

    Center Bed as origin (#4)
    - Add option to use the bed's center as origin
    - Improve first move to print start
    - Add option to set printing acceleration

--- _tools/lin_advance/k-factor.js
+++ _tools/lin_advance/k-factor.js
@@ -28,9 +28,11 @@ function genGcode() {
       SPEED_SLOW = parseInt(document.getElementById('SLOW_SPEED').value),
       SPEED_FAST = parseInt(document.getElementById('FAST_SPEED').value),
       SPEED_MOVE = parseInt(document.getElementById('MOVE_SPEED').value),
+      ACCELERATION = parseInt(document.getElementById('PRINT_ACCL').value),
       RETRACT_DIST = parseFloat(document.getElementById('RETRACTION').value),
       BED_X = parseInt(document.getElementById('BEDSIZE_X').value),
       BED_Y = parseInt(document.getElementById('BEDSIZE_Y').value),
+      NULL_CENTER = document.getElementById('CENTER_NULL').checked,
       HEIGHT_LAYER = parseFloat(document.getElementById('LAYER_HEIGHT').value),
       EXT_MULT = parseFloat(document.getElementById('EXTRUSION_MULT').value),
       START_K = parseInt(document.getElementById('K_START').value),
@@ -50,19 +52,19 @@ function genGcode() {
   // calculate some values for later use
   var RANGE_K = END_K - START_K,
       LINE_WIDTH = NOZZLE_DIAMETER * NOZZLE_LINE_RATIO,
-      PRINT_SIZE_Y = (RANGE_K / STEP_K * LINE_SPACING) + 25,
-      PRINT_SIZE_X = (2 * LENGTH_SLOW) + LENGTH_FAST + 10,
-      CENTER_X = BED_X / 2,
-      CENTER_Y = BED_Y / 2,
-      PRIME_START_X = CENTER_X - LENGTH_SLOW - (0.5 * LENGTH_FAST) - 5,
+      PRINT_SIZE_Y = (RANGE_K / STEP_K * LINE_SPACING) + 25, // +25 with ref marking
+      PRINT_SIZE_X = (2 * LENGTH_SLOW) + LENGTH_FAST + (USE_PRIME ? 10 : 0),
+      CENTER_X = (NULL_CENTER ? 0 : BED_X / 2),
+      CENTER_Y = (NULL_CENTER ? 0 : BED_Y / 2),
+      PRIME_START_X = CENTER_X - LENGTH_SLOW - (0.5 * LENGTH_FAST) - (USE_PRIME ? 5 : 0),
       PRIME_START_Y = CENTER_Y - (PRINT_SIZE_Y / 2),
       PRIME_END_X = PRIME_START_X,
       PRIME_END_Y = CENTER_Y + (PRINT_SIZE_Y / 2),
-      REF1_START_X = CENTER_X - (0.5 * LENGTH_FAST) + 5,
-      REF2_START_X = CENTER_X + (0.5 * LENGTH_FAST) + 5,
-      REF_START_Y = (PRINT_SIZE_Y / 2) + CENTER_Y - 20,
-      REF_END_Y = (PRINT_SIZE_Y / 2) + CENTER_Y,
-      PAT_START_X = CENTER_X - (0.5 * LENGTH_FAST) - LENGTH_SLOW + 5,
+      REF1_START_X = CENTER_X - (0.5 * LENGTH_FAST) + (USE_PRIME ? 5 : 0),
+      REF2_START_X = CENTER_X + (0.5 * LENGTH_FAST) + (USE_PRIME ? 5 : 0),
+      REF_START_Y = CENTER_Y + (PRINT_SIZE_Y / 2) - 20,
+      REF_END_Y = CENTER_Y + (PRINT_SIZE_Y / 2),
+      PAT_START_X = CENTER_X - (0.5 * LENGTH_FAST) - LENGTH_SLOW + (USE_PRIME ? 5 : 0),
       PAT_START_Y = CENTER_Y - (PRINT_SIZE_Y / 2);
 
   // force height of textarea to td cell Height
@@ -119,8 +121,8 @@ function genGcode() {
                                               ';\n' +
                                               '; Created: ' + new Date() + '\n' +
                                               '; Settings:\n' +
-                                              '; Print Size X = ' + PRINT_SIZE_X + ' mm\n' +
-                                              '; Print Size Y = ' + PRINT_SIZE_Y + ' mm\n' +
+                                              '; Print Size X = ' + FIT_WIDTH + ' mm\n' +
+                                              '; Print Size Y = ' + FIT_HEIGHT + ' mm\n' +
                                               '; Print Rotation = ' + PRINT_DIR + ' degree\n' +
                                               '; Print Pattern = ' + (ALT_PATTERN ? "Alternate" : "Standard") + '\n' +
                                               '; Print Frame = ' + (USE_FRAME ? "true" : "false") + '\n' +
@@ -131,14 +133,16 @@ function genGcode() {
                                               '; Nozzle Diameter = ' + NOZZLE_DIAMETER + ' mm\n' +
                                               '; Nozzle Temperature = ' + NOZZLE_TEMP + ' °C\n' +
                                               '; Nozzle / Line Ratio = ' + NOZZLE_LINE_RATIO + '\n' +
-                                              '; Bed Temperature = ' +BED_TEMP + ' °C\n' +
+                                              '; Bed Temperature = ' + BED_TEMP + ' °C\n' +
                                               '; Slow Printing Speed = ' + SPEED_SLOW + ' mm/min\n' +
                                               '; Fast Printing Speed = ' + SPEED_FAST + ' mm/min\n' +
+                                              '; Printing Acceleration = ' + ACCELERATION + ' mm/s^2\n' +
                                               '; Movement Speed = ' + SPEED_MOVE + ' mm/min\n' +
                                               '; Use BL = ' + (document.getElementById('USE_BL').checked ? "true" : "false") + '\n' +
                                               '; Retraction Distance = ' + RETRACT_DIST + ' mm\n' +
                                               '; Bed Size X = ' + BED_X + ' mm\n' +
                                               '; Bed Size Y = ' + BED_Y + ' mm\n' +
+                                              '; Origin Bed Center = ' + (NULL_CENTER ? "true" : "false") + '\n' +
                                               '; Layer Height = ' + HEIGHT_LAYER + ' mm\n' +
                                               '; Extrusion Multiplier = ' + EXT_MULT + '\n' +
                                               '; Starting Value K-Factor = ' + START_K + '\n' +
@@ -161,14 +165,10 @@ function genGcode() {
 
   document.getElementById('textarea').value += 'M109 S' + NOZZLE_TEMP + ' ; block waiting for nozzle temp\n' +
                                                'G21 ; set units to millimeters\n' +
-                                               'M204 P500 ; lower acceleration to 500mm/s2\n' +
+                                               'M204 P' + ACCELERATION + ' ; set acceleration\n' +
                                                'G90 ; use absolute coordinates\n' +
                                                'M83 ; use relative distances for extrusion\n' +
-                                               ';\n' +
-                                               '; reset extruder and go to layer height\n' +
-                                               ';\n' +
-                                               'G92 E0 ; reset extruder distance\n' +
-                                               'G1 Z' + HEIGHT_LAYER + ' F' + SPEED_SLOW + '\n';
+                                               'G92 E0 ; reset extruder distance\n';
 
   // Prime nozzle if activated
   if (USE_PRIME) {
@@ -177,7 +177,8 @@ function genGcode() {
                                                  ';\n' +
                                                  'G1 X' + Math.round10(rotateX(PRIME_START_X, CENTER_X, PRIME_START_Y, CENTER_Y, PRINT_DIR), -4) +
                                                    ' Y' + Math.round10(rotateY(PRIME_START_X, CENTER_X, PRIME_START_Y, CENTER_Y, PRINT_DIR), -4) +
-                                                   ' F' + SPEED_MOVE + '\n' +
+                                                   ' F' + SPEED_MOVE + ' ; move to prime start\n' +
+                                                 'G1 Z' + HEIGHT_LAYER + ' F' + SPEED_SLOW + ' ; move to layer height\n' +
                                                  'G1 X' + Math.round10(rotateX(PRIME_END_X, CENTER_X, PRIME_END_Y, CENTER_Y, PRINT_DIR), -4) +
                                                    ' Y' + Math.round10(rotateY(PRIME_END_X, CENTER_X, PRIME_END_Y, CENTER_Y, PRINT_DIR), -4) +
                                                    ' E' + EXT_PRIME1 + ' F' + SPEED_SLOW + ' ; prime nozzle\n' +
@@ -198,6 +199,7 @@ function genGcode() {
                                                  'G1 X' + Math.round10(rotateX(PAT_START_X - LINE_WIDTH, CENTER_X, PAT_START_Y - 3, CENTER_Y, PRINT_DIR), -4) +
                                                    ' Y' + Math.round10(rotateY(PAT_START_X - LINE_WIDTH, CENTER_X, PAT_START_Y - 3, CENTER_Y, PRINT_DIR), -4) +
                                                    ' F' + SPEED_MOVE + ' ; move to frame start\n' +
+                                                 (USE_PRIME ? '' : 'G1 Z' + HEIGHT_LAYER + ' F' + SPEED_SLOW + ' ; move to layer height\n') +
                                                  (USE_PRIME ? 'G1 E' + RETRACT_DIST + '\n' : '') +
                                                  'G1 X' + Math.round10(rotateX(PAT_START_X - LINE_WIDTH, CENTER_X, PAT_START_Y + PRINT_SIZE_Y - 22, CENTER_Y, PRINT_DIR), -4) +
                                                    ' Y' + Math.round10(rotateY(PAT_START_X - LINE_WIDTH, CENTER_X, PAT_START_Y + PRINT_SIZE_Y - 22, CENTER_Y, PRINT_DIR), -4) +
@@ -226,7 +228,9 @@ function genGcode() {
   }
 
   // insert a retract if no prime and no frame
-  (!USE_PRIME && !USE_FRAME ? document.getElementById('textarea').value += 'G1 E-' + RETRACT_DIST + '\n' : '');
+  if (!USE_PRIME && !USE_FRAME) {
+    document.getElementById('textarea').value += 'G1 E-' + RETRACT_DIST + '\n';
+  }
 
   // generate the k-factor test pattern
   document.getElementById('textarea').value += ';\n' +
@@ -236,6 +240,7 @@ function genGcode() {
                                                'G1 X' + Math.round10(rotateX(PAT_START_X, CENTER_X, PAT_START_Y, CENTER_Y, PRINT_DIR), -4) +
                                                  ' Y' + Math.round10(rotateY(PAT_START_X, CENTER_X, PAT_START_Y, CENTER_Y, PRINT_DIR), -4) +
                                                  ' F' + SPEED_MOVE + ' ; move to pattern start\n' +
+                                                (USE_PRIME || USE_FRAME ? '' : 'G1 Z' + HEIGHT_LAYER + ' F' + SPEED_SLOW + ' ; move to layer height\n') +
                                                 (ALT_PATTERN ? 'G1 E' + RETRACT_DIST + '\n' : '');
   var j = 0,
       k = 0;
@@ -317,7 +322,7 @@ function genGcode() {
                                                ';\n' +
                                                'M104 S0 ; turn off hotend\n' +
                                                'M140 S0 ; turn off bed\n' +
-                                               'G1 Z30 Y200 F' + SPEED_MOVE + ' ; move away from the print\n' +
+                                               'G1 Z30 X' + (NULL_CENTER ? BED_X / 2 : BED_X) + ' Y' + (NULL_CENTER ? BED_Y / 2 : BED_Y) + ' F' + SPEED_MOVE + ' ; move away from the print\n' +
                                                'M84 ; disable motors\n' +
                                                'M502 ; resets parameters from ROM (for those who do not have an EEPROM)\n' +
                                                'M501 ; resets parameters from EEPROM (preferably)\n' +
