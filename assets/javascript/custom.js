a9d706772de96bf0afd017efe4131f82ade95b83
Luu Lac <45380455+shitcreek@users.noreply.github.com>
2019-07-05 00:27:55 -0500

    improve drop down menu visibility and minor edits (#227)

--- assets/javascript/custom.js
+++ assets/javascript/custom.js
@@ -1,19 +1,19 @@
-$( document ).ready(function() {
+$(function() {
     /**
      * Dynamically build the table of contents
      */
     $("#toc").tocify({
-        selectors: (typeof toc_selectors != 'undefined') ? toc_selectors : 'h1,h2,h3',
-        scrollTo: 65,
-        extendPage: false,
-        hashGenerator: 'pretty',
+      selectors: (typeof toc_selectors != 'undefined') ? toc_selectors : 'h1,h2,h3',
+      scrollTo: 65,
+      extendPage: false,
+      hashGenerator: 'pretty',
     });
 
     /**
      * SEO optimization: mark all external link as nofollow
      */
     $('a:not(:has(img))').filter(function() {
-        return this.hostname && this.hostname !== location.hostname;
+      return this.hostname && this.hostname !== location.hostname;
     }).attr('target', '_blank').attr('rel', 'nofollow');
 
     /**
@@ -32,4 +32,24 @@ $( document ).ready(function() {
       if (this.href.endsWith(url) && url.length > 2)
         $(this).addClass('here').parents('li').addClass('here');
     });
+
+    //responsive submenu - shifts to left on smaller window
+    //bootstrap's pull-left is right and vice versa
+    var minWindowWidth = 768, maxWindowWidth = 1100;
+
+    function shiftSubMenu(){
+      if ($(window).width() >= minWindowWidth && $(window).width() <= maxWindowWidth ){
+        $('.dropdown-menu').addClass('pull-right');
+        $('.dropdown-submenu .dropdown-menu').addClass('flip-left');
+      }
+      else {
+        $('.dropdown-menu').removeClass('pull-right');
+        $('.dropdown-submenu .dropdown-menu').removeClass('flip-left');
+      }
+    };
+
+    shiftSubMenu();
+
+    $(window).resize(shiftSubMenu);
+
 });
