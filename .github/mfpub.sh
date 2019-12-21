490eace8163f780b4e368be592b2df3232e2ef98
Scott Lahteine <github@thinkyhead.com>
2019-12-21 15:24:20 -0600

    Auto-publish v1

---
+++ .github/mfpub.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#
+# mfpub
+# Use Jekyll to publich Marlin Documentation to the live site.
+#
+
+# build the site statically and proof it
+bundle exec jekyll build --profile --trace --no-watch
+bundle exec htmlproofer ./_site --only-4xx --allow-hash-href --check-favicon --check-html --url-swap ".*marlinfw.org/:/"
+
+# Sync the built site into a temporary folder
+TMPFOLDER=$( mktemp -d )
+rsync -av _site/ ${TMPFOLDER}/
+
+# Copy built-site into the gh-pages branch
+git checkout gh-pages || { echo "Something went wrong!"; exit 1; }
+rsync -av ${TMPFOLDER}/ ./
+
+# Commit and push the new live site directly
+git add --all
+git commit --message "Built from ${COMMIT}"
+git push
+
+# remove the temporary folder
+rm -rf ${TMPFOLDER}
+
+# Go back to the branch we started from
+git checkout master
