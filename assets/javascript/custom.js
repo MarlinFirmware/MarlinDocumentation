d1efe831387863b07663d4f856c15d01f543abaf
ellensp <530024+ellensp@users.noreply.github.com>
2024-08-06 21:43:59 +1200

    use Modal Images

--- assets/javascript/custom.js
+++ assets/javascript/custom.js
@@ -193,3 +193,29 @@ $(function() {
   // Fire the singleton init on document.ready
   jekyllSearch.init();
 });
+
+// create references to the modal...
+var modal = document.getElementById('myModal');
+// to all images -- note I'm using a class!
+var images = document.getElementsByClassName('myImages');
+// the image in the modal
+var modalImg = document.getElementById("img01");
+// and the caption in the modal
+var captionText = document.getElementById("caption");
+
+// Go through all of the images with our custom class
+for (var i = 0; i < images.length; i++) {
+  var img = images[i];
+  // and attach our click listener for this image.
+  img.onclick = function(evt) {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+  }
+}
+
+var span = document.getElementsByClassName("modalclose")[0];
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
